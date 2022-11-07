cb8df3aa8d4aa8f151e2d45067d21f782e7960eb
Ezequiel Aldana <ezequielaldana77@gmail.com>
2022-11-07 10:46:34 -0300

    curso avanzado2

--- funciones.js
+++ funciones.js
@@ -6,3 +6,9 @@ function doGet()
 function obtenerDatosHTML(nombre){
    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+function obtenerContactos()
+{
+let hoja = SpreadsheetApp.openById('1EyF9A6jJZEaEETzHmLYO-IBDRYgk_esChjjVBwMDdwE').getSheetByName('datos');
+let datos= hoja.getDataRange().getValues();
+return datos;
+}

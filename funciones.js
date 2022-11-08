2b6126a677256a9f726d3aa26566a708b7eb8e41
Ezequiel Aldana <ezequielaldana77@gmail.com>
2022-11-08 16:52:11 -0300

    curso avanzado 5

--- funciones.js
+++ funciones.js
@@ -1,14 +1,29 @@
-function doGet()
+const HOJA = SpreadsheetApp.openById('1EyF9A6jJZEaEETzHmLYO-IBDRYgk_esChjjVBwMDdwE').getActiveSheet();
+
+function doGet(datos)
 {
+    insertarContacto(datos.parameter.nombre, datos.parameter.correo, datos.parameter.telefono);
     return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google App Script');
 }
 
-function obtenerDatosHTML(nombre){
+function doPost(datos)
+{
+    insertarContacto(datos.parameter.nombre, datos.parameter.correo, datos.parameter.telefono);    
+    return HtmlService.createTemplateFromFile('web').evaluate().setTitle('Agenda Google App Script');
+
+}
+
+function obtenerDatosHTML(nombre)
+{
    return HtmlService.createHtmlOutputFromFile(nombre).getContent();
 }
+
 function obtenerContactos()
 {
-let hoja = SpreadsheetApp.openById('1EyF9A6jJZEaEETzHmLYO-IBDRYgk_esChjjVBwMDdwE').getSheetByName('datos');
-let datos= hoja.getDataRange().getValues();
-return datos;
+return HOJA.getDataRange().getValues();
+}
+
+function insertarContacto(nombre, correo, telefono)
+{
+    HOJA.appendRow([nombre,correo,telefono]);
 }

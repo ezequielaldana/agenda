38541f9e68e7cfafa79911d869ec1b1b0af806d8
Ezequiel Aldana <ezequielaldana77@gmail.com>
2022-11-14 07:58:33 -0300

    curso avanzado 9

--- funciones.js
+++ funciones.js
@@ -41,7 +41,7 @@ function modificarContacto(numFila,datos)
 
 function importarContactos()
 {
-    let url = 'https://randomuser.me/api/?results=5&inc=name,email,phone';
+    let url = 'https://randomuser.me/api/?results=5&inc=name,email,phone,picture';
     let respuesta = UrlFetchApp.fetch(url).getContentText();
     let datos = JSON.parse(respuesta)
     datos.results.forEach(insertarContactoJSON);
@@ -49,5 +49,5 @@ function importarContactos()
 
 function insertarContactoJSON(contacto)
 {
-  HOJA.appendRow([contacto.name.first,contacto.name.last,contacto.email, contacto.phone]);
+  HOJA.appendRow([contacto.name.first,contacto.name.last,contacto.email, contacto.phone, contacto.picture.medium]);
 }
